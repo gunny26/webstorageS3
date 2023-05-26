c9c8e85b44e393ffe3b53bd51cfe86955df94dfe
Arthur Messner <arthur.messner@gmail.com>
2023-05-26 20:27:34 +0200

    automatic pre latest built commit

--- webstorageS3/StorageClientS3.py
+++ webstorageS3/StorageClientS3.py
@@ -116,9 +116,7 @@ class StorageClient:
 
 
     def _check_bucket(self):
-        print(self._client.list_buckets())
         buckets = [bucket["Name"] for bucket in self._client.list_buckets()["Buckets"]]
-        print(buckets)
         if self._bucket_name not in buckets:
             self._logger.error(f"Bucket {self._bucket_name} does not exist")
             self._logger.debug(f"list of buckets {self._client.list_buckets()}")
